a94eb648165046df37564e337db0c1d974474b41
jorgelucas <jorgelucasl91@gmail.com>
2021-07-07 17:32:11 -0300

    [FEATURE]  Práticando DataBind

---
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styles: []
+})
+export class DataBindingComponent {
+  public contadorCliques: number = 0;
+  public urlImage: string = "https://angular.io/assets/images/logos/angular/angular.svg";
+  public nome: string = "";
+  public nomeModule: string = "";
+
+  adicionarClique() {
+    this.contadorCliques++;
+  }
+  zerarContador() {
+    this.contadorCliques = 0;
+  }
+  keyUp(event: any) {
+    this.nome = event.target.value;
+  }
+}
